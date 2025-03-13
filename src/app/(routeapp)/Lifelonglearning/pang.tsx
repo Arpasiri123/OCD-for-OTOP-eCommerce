e8468787c60ffb8aa2fa14a3d2a290127fd863a0
FortunarMapping <thanakorn.chaip@ku.th>
2025-03-13 20:54:45 +0700

    first commit

---
+++ src/app/(routeapp)/Lifelonglearning/pang.tsx
@@ -0,0 +1,10 @@
+import Lifelonglearning from '@/components/Lifelonglearning'
+import React from 'react'
+
+export default function pang() {
+  return (
+    <div>
+    <Lifelonglearning/>
+    </div>
+  )
+}
---
+++ src/app/(routeapp)/contact/pang.tsx
@@ -0,0 +1,10 @@
+import Contact from '@/components/Contact'
+import React from 'react'
+
+export default function pang() {
+  return (
+    <div>
+        <Contact/>
+    </div>
+  )
+}
